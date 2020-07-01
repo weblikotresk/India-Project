3b12aac61585d524be627a604368256d9b56491b
weblikotresk <60964556+weblikotresk@users.noreply.github.com>
2020-07-01 12:31:43 +0300

    lmao

--- india/script.js
+++ india/script.js
@@ -1,40 +1,42 @@
 let today = new Date(),
-tommorrow = new Date(Date.now() + 63113852000);
-console.log(today.getFullYear());
-console.log(tommorrow.getFullYear()-1);
+tommorrow = new Date(Date.now() + 63113852000),
+// today_h = today.getHours,
+// today_m = today.getMinutes;
+today_time = `${today.getHours()}:${today.getMinutes()}`;
+console.log(today_time);
 (function($) {
     $(function() {
-    
+        
         $('.book_select').styler();
 
         $('.name_input').styler();
         
-//tommorrow = new Date(Date.parse(today.getFullYear() + 3));
+        tommorrow = new Date(Date.parse(today.getFullYear() + 3));
 
-        $('#date').datepicker({
-            
-            // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня, а также не дальше, чем на 2 года вперёд.
-            
+         $('#date').datepicker({
+             // Можно выбрать тольо даты, идущие за сегодняшним днем, включая сегодня, а также не дальше, чем на 2 года вперёд.
             minDate: today,
             maxDate: tommorrow
 
-        });
-        // Сделаем неактивными воскресенье и субботу
-        let disabledDays = [0, 6];
+         });
+         // Сделаем неактивными воскресенье и субботу
+         let disabledDays = [0, 6];
 
-        $('#date').datepicker({
+         $('#date').datepicker({
             
-            onRenderCell: function (date, cellType) {
-                if (cellType == 'day') {
-                    var day = date.getDay(),
-                        isDisabled = disabledDays.indexOf(day) != -1;
+             onRenderCell: function (date, cellType) {
+                 if (cellType == 'day') {
+                     var day = date.getDay(),
+                         isDisabled = disabledDays.indexOf(day) != -1;
 
-                    return {
-                        disabled: isDisabled
-                    }
-                }
-            }
-        })
+                     return {
+                         disabled: isDisabled
+                     }
+                 }
+             }
+         })
+
+        
     });
     })(jQuery);
 function validMail() {
@@ -55,43 +57,47 @@ function validPhone() {
     else document.getElementById('message_phone').innerHTML ='';
     return valid;
 }  
-function validDate(){
-    console.log(today.getFullYear(), tommorrow.getFullYear()-1,tommorrow.getFullYear() )
-    //let re = new RegExp('/^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(2020|2021|2022)[\s\D]([01]?[0-9]|2[0-3]):[0-5][0-9]/');
-    let re = /(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.2020[\s\D]([01]?[0-9]|2[0-3]):[0-5][0-9]/;
+ function validDate(){
+     console.log(today.getFullYear(), tommorrow.getFullYear()-1,tommorrow.getFullYear() )
+     //let re = new RegExp('/^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.(2020|2021|2022)[\s\D]([01]?[0-9]|2[0-3]):[0-5][0-9]/');
+     let re = /(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.2020[\s\D]([01]?[0-9]|2[0-3]):[0-5][0-9]/;
     // re = new RegExp(re);
-    // console.log(re);
-    var myDate = document.getElementById('date').value;
-    console.log(myDate);
+     // console.log(re);
+     var myDate = document.getElementById('date').value;
+     console.log(myDate);
     var valid = re.test(myDate);
-    if (valid == false){
-        console.log('false');
+     if (valid == false){
+         console.log('false');
         document.getElementById('message_date').innerHTML = 'Please, enter correct date and time.';
-    } 
+     } 
     else{
-        console.log('true');
+         console.log('true');
         document.getElementById('message_date').innerHTML ='';
     } 
-    return valid;
+     return valid;
 }
 function validTime(){
-    var re = /([01]?[0-9]|2[0-3]):[0-5][0-9]/;
-    var myTime = document.getElementById('time').value;
-    var valid = re.test(myTime);
-    if (valid == false) document.getElementById('message_time').innerHTML = 'Your time is incorrect. Try again.';
-    else document.getElementById('message_time').innerHTML ='';
-    return valid;
+     var re = /([01]?[0-9]|2[0-3]):[0-5][0-9]/;
+     var myTime = document.getElementById('time').value;
+     var valid = re.test(myTime);
+     if (valid == false) document.getElementById('message_time').innerHTML = 'Your time is incorrect. Try again.';
+     else document.getElementById('message_time').innerHTML ='';
+     return valid;
 }
 let submit_btn = document.getElementById('submit_btn');
-
 submit_btn.onclick = function(){
-    validMail()
+    validMail() 
     validPhone()
-    validDate()
-    // console.log(document.getElementsByClassName('datepicker-here')[0].value)
-    // if (document.getElementsByClassName('datepicker-here')[0].innerHTML == ''){
-    //     document.getElementById('message_date').innerHTML = 'Please, enter date and time.';
-    // }else{
-    //     document.getElementById('message_date').innerHTML = '';
-    // }
+     //validDate()
+    // validTime();
+     console.log(document.getElementById('time').value)
 }
+// let time_input = document.getElementById('time');
+// time_input.onchange = function(){
+//     let re_fi = /([01]?[0-9]|2[0-3])/;
+//     let valid = re_fi.test(time_input.value);
+//     if(valid && ){
+//         time_input.innerHTML = `${time_input.innerHTML}:`;
+//     }
+// }
+
